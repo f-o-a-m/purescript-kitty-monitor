cf9c78f78fa78bb880a40cc1c2742a3a7deee986
kejace <kejace@gmail.com>
2017-11-03 16:08:38 -0400

    Update package.json

--- src/Main.purs
+++ src/Main.purs
@@ -3,17 +3,17 @@ module Main where
 import Prelude
 
 import Contracts.EthereumWhite as EW
-import Contracts.DecentrEx as DX
 import Control.Monad.Aff (runAff_)
+import Control.Monad.Aff.AVar (makeEmptyVar, putVar, takeVar)
 import Control.Monad.Aff.Class (liftAff)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Eff.Console (CONSOLE, log, logShow)
-import Control.Monad.Reader (ReaderT)
+import Control.Monad.Eff.Console (log, logShow)
+import Control.Monad.Reader (ReaderT(..))
 import Data.Either (either)
 import Network.Ethereum.Web3.Contract (EventAction(..), event)
 import Network.Ethereum.Web3.Provider (runWeb3)
-import Network.Ethereum.Web3.Types (ETH, Web3(..), Address(..), HexString(..))
+import Network.Ethereum.Web3.Types (Web3, Address(Address), HexString(HexString))
 import Utils (HttpProvider)
 
 
@@ -22,30 +22,79 @@ import Utils (HttpProvider)
 --                       | e
 --                       ) Unit
 main = do
+  ethereumWhite
 
+ethereumWhite = do
   let ewAddress = Address <<< HexString $ "39e505e1518813ab3834d57d06c22b2e5a7fb9f2"
-  let dxAddress = Address <<< HexString $ "bf29685856fae1e228878dfb35b280c0adcc3b05"
+  void <<< runAff_ (\e -> log $ either show (\_ -> "i'm back") e) $ do 
+    varTransfer <- makeEmptyVar 
+    varMine <- makeEmptyVar
+    varPoS <- makeEmptyVar
+    varAD <- makeEmptyVar
+    varApproval <- makeEmptyVar
+    varSponsored <- makeEmptyVar
 
-  log $ "Hello EthereumWhite at " <> show ewAddress
-  
-  runAff_ (\e -> log $ either show (\_ -> "i'm back (EW)") e) $ liftAff $ runWeb3 $
-    -- Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event ewAddress $ \(EW.Transfer _from _to _value) -> do
-      liftEff $ logShow $ "EW.Transfer: " 
-                       <> show _from 
-                       <> " -> " 
-                       <> show _to <> ": " <> show _value
-      pure TerminateEvent :: ReaderT _ (Web3 HttpProvider _) _
+    liftEff $ log $ "Hello EthereumWhite at " <> show ewAddress
+    _ <- runWeb3 $ do
 
-  log $ "Hello DecentrEx at " <> show dxAddress
+      void <<< event ewAddress $ \e -> do
+        liftEff $ logShow $ e
+        pure TerminateEvent :: ReaderT _ (Web3 HttpProvider _) _
+     
+      void <<< event ewAddress $ \e@(EW.Transfer _from _to _value) -> do
+        _ <- liftAff $ putVar _value varTransfer
+        liftEff $ logShow $ e
+        pure TerminateEvent :: ReaderT _ (Web3 HttpProvider _) _
 
-  runAff_ (\e -> log $ either show (\_ -> "i'm back (DX)") e) $ liftAff $ runWeb3 $
-    --  Trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, address get, address give);
-    event dxAddress $ \(DX.Trade _tokenGet _amountGet _tokenGive _amountGive _get _give) -> do
-      liftEff $ logShow $ "DX.Trade: " 
-                       <> show _tokenGet <> " : " <> show _amountGet
-                       <> " -> " 
-                       <> show _tokenGive <> " : " <> show _amountGive
-                       <> " :: "
-                       <> show _get <> " -> " <> show _give
-      pure TerminateEvent :: ReaderT _ (Web3 HttpProvider _) _
+      void <<< event ewAddress $ \e@(EW.Mine _address _reward) -> do
+        _ <- liftAff $ putVar _reward varMine
+        liftEff $ logShow e
+        pure TerminateEvent :: ReaderT _ (Web3 HttpProvider _) _
+
+      void <<< event ewAddress $ \e@(EW.MinePoS _address _rewardPoS) -> do
+        _ <- liftAff $ putVar _rewardPoS varPoS
+        liftEff $ logShow e
+        pure TerminateEvent :: ReaderT _ (Web3 HttpProvider _) _
+
+      void <<< event ewAddress $ \e@(EW.MineAD _address _rewardAD) -> do
+        _ <- liftAff $ putVar _rewardAD varAD
+        liftEff $ logShow e
+        pure TerminateEvent :: ReaderT _ (Web3 HttpProvider _) _
+
+      void <<< event ewAddress $ \e@(EW.Approval _owner _spender _value) -> do
+        _ <- liftAff $ putVar _value varApproval
+        liftEff $ logShow e
+        pure TerminateEvent :: ReaderT _ (Web3 HttpProvider _) _
+
+      event ewAddress $ \e@(EW.SponsoredLink _note) -> do
+        _ <- liftAff $ putVar _note varSponsored
+        liftEff $ logShow e
+        pure TerminateEvent :: ReaderT _ (Web3 HttpProvider _) _
+
+    printVar varTransfer
+    printVar varMine
+    printVar varPoS
+    printVar varAD
+    printVar varApproval
+ --   printVar varSponsored
+
+
+--decentrEx = do 
+--  let dxAddress = Address <<< HexString $ "bf29685856fae1e228878dfb35b280c0adcc3b05"
+
+--   log $ "Hello DecentrEx at " <> show dxAddress
+--   runAff_ (\e -> log $ either show (\_ -> "i'm back (DX)") e) $ liftAff $ runWeb3 $
+--     --  Trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, address get, address give);
+--     event dxAddress $ \(DX.Trade _tokenGet _amountGet _tokenGive _amountGive _get _give) -> do
+--       liftEff $ logShow $ "DX.Trade: " 
+--                        <> show _tokenGet <> " : " <> show _amountGet
+--                        <> " -> " 
+--                        <> show _tokenGive <> " : " <> show _amountGive
+--                        <> " :: "
+--                        <> show _get <> " -> " <> show _give
+--       pure TerminateEvent :: ReaderT _ (Web3 HttpProvider _) _
+
+ -- printVar :: (Show a) => a -> Eff ()
+printVar var = do
+  val <- takeVar var
+  liftEff $ log $ "Event: " <> show val
