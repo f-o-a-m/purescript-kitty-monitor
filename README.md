6c244072f218a5f5d6e1c25ea29af3a60d210cbf
kejace <kejace@gmail.com>
2017-12-04 11:58:08 -0500

    Tracks kittens on a block-per-block basis

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ arbitrary abi + ps-web3
 ## Build instructions
 ```bash
 > npm i
-> NODE_URL="http://geth-mainnet-deploy.foam.svc.cluster.local:8545" pulp run 
+> NODE_URL="http://parity-mainnet.foam.svc.cluster.local:8545" pulp run
 ```
--- src/Main.purs
+++ src/Main.purs
@@ -7,6 +7,7 @@ import Contracts.CryptoKitties as CK
 import Contracts.DecentrEx as DX
 import Contracts.ERC721 as T
 import Contracts.EthereumWhite as EW
+import Contracts.KittyCore as KC
 import Contracts.LightOracle as LO
 import Contracts.OmiseGo as OG
 import Control.Monad.Aff (Aff, attempt, launchAff_)
@@ -16,11 +17,12 @@ import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Unsafe (unsafePerformEff)
 import Control.Monad.Reader (ReaderT)
+import Control.Monad.Reader.Class (ask)
 import Data.Either (Either(Right, Left))
 import Data.Foldable (sequence_)
 import Data.Maybe (Maybe(..), fromJust)
 import Data.Traversable (for)
-import Network.Ethereum.Web3 (class IsAsyncProvider, CallMode(..), ETH, embed, mkAddress, mkHexString, uIntNFromBigNumber)
+import Network.Ethereum.Web3 (class IsAsyncProvider, CallMode(..), Change(..), ETH, fromHexString, mkAddress, mkHexString)
 import Network.Ethereum.Web3.Contract (EventAction(..), event)
 import Network.Ethereum.Web3.Provider (runWeb3)
 import Network.Ethereum.Web3.Types (Web3)
@@ -59,18 +61,20 @@ spork theEvent provider = do
     Right a -> do
       liftEff $ log "got proper response from JSON-RPC"
 
-logEvent :: forall proxy a b c
+logEvent :: forall proxy a c
           . (Show a)
          => (Monad c)
-         => proxy a -> a -> ReaderT b c EventAction
+         => proxy a -> a -> ReaderT Change c EventAction
 logEvent _ e = pure $ unsafePerformEff $ (log $ "logEvent: " <> show e) >>= (const $ pure ContinueEvent)
 
 -- mainnet
 cryptoKitties :: EventLog
 cryptoKitties = do
   let ckAddress = unsafePartial fromJust $ mkAddress =<< mkHexString "0xC7af99Fe5513eB6710e6D5f44F9989dA40F27F26"
-  liftEff $ log $ "Hello CryptoKitties at " <> show ckAddress
   aaAddress <- CK.eth_nonFungibleContract ckAddress Nothing Latest
+
+  liftEff <<< log $ "Hello CryptoKitties at " <> show ckAddress
+  liftEff <<< log $ "ERC721 is at " <> show aaAddress 
 
   sequence_
     [ event ckAddress $ (logEvent $ Proxy :: Proxy CK.AuctionCreated)
@@ -78,9 +82,17 @@ cryptoKitties = do
     , event ckAddress $ (logEvent $ Proxy :: Proxy CK.AuctionCancelled)
     , event ckAddress $ (logEvent $ Proxy :: Proxy CK.Pause)
     , event ckAddress $ (logEvent $ Proxy :: Proxy CK.Unpause)
-    , event aaAddress $ (logEvent $ Proxy :: Proxy T.Transfer)
+   -- , event aaAddress $ (logEvent $ Proxy :: Proxy T.Transfer)
     , event aaAddress $ (logEvent $ Proxy :: Proxy T.Approval)
     ]
+
+  _ <- event aaAddress $ \(e@KC.Transfer r) -> do
+    (Change change) <- ask
+    liftEff <<< log $ "logEvent: " <> show e
+    let blockNum = BlockNumber $ fromHexString change.blockNumber
+    tokens <- lift $ T.eth_balanceOf aaAddress Nothing blockNum r.to
+    liftEff <<< log $ "\tTokens for " <> show r.to <> " is " <> show tokens
+    pure ContinueEvent
 
   liftEff $ log $ "Bye CryptoKitties at " <> show ckAddress
 
